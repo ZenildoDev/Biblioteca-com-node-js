eb8c694740e06cce6e4ab09e169b1b832c5368ce
Zenilo Bismark <zenildobismarkzbrl@gmail.com>
2026-02-16 10:57:13 -0300

    Segundo commit envio da logica do prejeto criacao dos filtros de palavras e paragrafos

--- src/index.js
+++ src/index.js
@@ -4,5 +4,31 @@ const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
-    console.log(texto);
+    quebraEmParagrafos(texto);
 })
+
+function quebraEmParagrafos(texto){
+    const paragrafos = texto.toLowerCase().split('\n')
+    const contagem = paragrafos.map((paragrafo) => {
+        return verificaPalavrasDuplicadas(paragrafo);
+    })
+
+    console.log(contagem);
+}
+
+function limpaPalavras(palavra) {
+  return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+}
+
+function verificaPalavrasDuplicadas(texto){
+    const listaPalavras = texto.split(' ');
+    const resultado = {};
+    listaPalavras.forEach(palavra => {
+        if(palavra.length >= 3) {
+            const palavraLimpa = limpaPalavras(palavra); //isso remove caracteres colados  nas palavras
+            resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1;
+        }
+    });
+
+    return resultado;
+}
